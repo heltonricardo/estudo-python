a2681f832d3caed2fc3870c2df6517560ca201e8
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-11 09:04:55 -0300

    Create p13.py

---
+++ Entrada/p13.py
@@ -0,0 +1,15 @@
+mt = float(input('Insira valor em metros: '))
+km = mt / 1000
+hm = mt / 100
+dam = mt / 10
+dm = mt * 10
+cm = mt * 100
+mm = mt * 1000
+print()
+print('Valor em kilometros..: {:>20}'.format(km))
+print('Valor em hectômetros.: {:>20}'.format(hm))
+print('Valor em decâmetros..: {:>20}'.format(dam))
+print('Valor em decímetros..: {:>20}'.format(dm))
+print('Valor em centímetros.: {:>20}'.format(cm))
+print('Valor em milímetros..: {:>20}'.format(mm))
+input
