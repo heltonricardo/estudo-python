9b9283072cc53a4f994318e052809eb180320dd5
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-14 12:09:20 -0300

    Create p21.py

---
+++ Entrada/p21.py
@@ -0,0 +1,9 @@
+import math
+n = int(input('Entre um valor inteiro: '))
+r = math.floor(math.sqrt(n))
+print('Raíz quadrada de {} é {}.'.format(n, r))
+input()
+
+# ou use
+# from math import sqrt, floor
+# r = floor(sqrt(n))
