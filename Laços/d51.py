7bcfc58a0680c825f5e238fe1140fdaa5a7eeeb3
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-19 10:49:53 -0300

    Create d51.py

---
+++ Laços/d51.py
@@ -0,0 +1,10 @@
+p = int(float(input('Entre o primeiro termo da PA ..: ')))
+r = int(float(input('Entre a razão da PA ...........: ')))
+
+i = 1
+while (i <= 10):
+    print(p, end=' ')
+    p += r
+    i += 1
+
+input()
