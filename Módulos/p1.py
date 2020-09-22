7ab815133c48d671b5b38b4e3072f230e5ba1243
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-22 11:30:33 -0300

    Create p1.py

---
+++ Módulos/p1.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+agora = datetime.now()
+
+print('agora ..............:', agora)
+print('agora.year .........:', agora.year)
+print('agora.month ........:', agora.month)
+print('agora.day ..........:', agora.day)
+print('agora.hour .........:', agora.hour)
+print('agora.minute .......:', agora.minute)
+print('agora.second .......:', agora.second)
+print('agora.microsecond ..:',agora.microsecond)
+input()
