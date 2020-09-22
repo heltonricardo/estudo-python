ae4d4f8d77bcc39e1c3c46598b8a34ade4b6f936
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-22 11:30:38 -0300

    Create p1.py

---
+++ Random/p1.py
@@ -0,0 +1,10 @@
+from random import randint as r
+from random import sample
+for i in range(20):
+    print(r(0,5), end=' ')
+
+print()
+print(sample(range(1, 11), 10))
+print(sample(range(1, 11), 10))
+print(sample(range(1, 11), 10))
+input()
