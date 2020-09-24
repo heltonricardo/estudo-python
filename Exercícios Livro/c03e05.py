341fcbf577ce1968a882f45035e214851a2b10fc
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-24 09:21:14 -0300

    Create c03e05.py

---
+++ Exercícios Livro/c03e05.py
@@ -0,0 +1,14 @@
+A = int(input('Entre inteiro para A: '))
+B = int(input('Entre inteiro para B: '))
+C = int(input('Entre inteiro para C: '))
+D = int(input('Entre inteiro para D: '))
+
+if(A % 2 == 0 and A % 3 == 0):
+    print('{:4} é divisível por 2 e 3.'.format(A))
+if(B % 2 == 0 and B % 3 == 0):
+    print('{:4} é divisível por 2 e 3.'.format(B))
+if(C % 2 == 0 and C % 3 == 0):
+    print('{:4} é divisível por 2 e 3.'.format(C))
+if(D % 2 == 0 and D % 3 == 0):
+    print('{:4} é divisível por 2 e 3.'.format(D))
+input()
