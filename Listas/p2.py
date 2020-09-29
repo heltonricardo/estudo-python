eb129023a110cd48a70256f1c9b46f16e85ba3d7
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-29 08:45:40 -0300

    Create p2.py

---
+++ Listas/p2.py
@@ -0,0 +1,15 @@
+val = list(range(1, 11))
+print('val = list(range(1, 11))')
+print('val =', val)
+
+val.sort(reverse=True)
+print('\nval.sort(reserve=True)')
+print('val =', val)
+
+val.sort()
+print('\nval.sort()')
+print('val =', val)
+
+print('\nTamanho -> len(val) =', len(val))
+
+input()
