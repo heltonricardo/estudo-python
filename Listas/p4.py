7f2ad20f647351c5d0084b48ba8daf6985d458b1
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-29 08:45:44 -0300

    Create p4.py

---
+++ Listas/p4.py
@@ -0,0 +1,10 @@
+A = [1, 2, 3, 4]
+print('A = [1, 2, 3, 4]')
+B = A
+print('B = A')
+
+print('print(B) =', B)
+print('\nB = A não cria uma cópia da lista. As listas nesse ponto estão ligadas.')
+print('O que for feito em A acontece em B e vice-versa')
+print()
+print('Para criar uma cópia: B = A[:]')
